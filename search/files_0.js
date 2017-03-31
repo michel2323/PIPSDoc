e35e5f25a72c950d2a0aacaeb28439bbb1a9f1b4
Michel Schanen <mschanen@anl.gov>
2017-03-31 13:19:38 -0500

    mainpage

--- search/files_0.js
+++ search/files_0.js
@@ -1,9 +1,4 @@
 var searchData=
 [
-  ['ampldata_5fnl_2ecpp',['AmplData_NL.cpp',['../_ampl_data___n_l_8cpp.html',1,'']]],
-  ['ampldata_5fnl_2ehpp',['AmplData_NL.hpp',['../_ampl_data___n_l_8hpp.html',1,'']]],
-  ['amplgenstochinput_2ecpp',['amplGenStochInput.cpp',['../ampl_gen_stoch_input_8cpp.html',1,'']]],
-  ['amplgenstochinput_2ehpp',['amplGenStochInput.hpp',['../ampl_gen_stoch_input_8hpp.html',1,'']]],
-  ['amplgenstochinput_5faddslack_2ecpp',['amplGenStochInput_AddSlack.cpp',['../ampl_gen_stoch_input___add_slack_8cpp.html',1,'']]],
-  ['amplgenstochinput_5faddslack_2ehpp',['amplGenStochInput_AddSlack.hpp',['../ampl_gen_stoch_input___add_slack_8hpp.html',1,'']]]
+  ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]]
 ];
